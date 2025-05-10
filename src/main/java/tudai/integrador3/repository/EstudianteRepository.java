d319ea2777f0ab7d173bc2caafee4dae0d7d7ee7
brostsimon <130936417+brostsimon@users.noreply.github.com>
2025-05-10 09:57:08 -0300

    Update EstudianteRepository.java

--- src/main/java/tudai/integrador3/repository/EstudianteRepository.java
+++ src/main/java/tudai/integrador3/repository/EstudianteRepository.java
@@ -5,23 +5,24 @@ import org.springframework.stereotype.Repository;
 import tudai.integrador3.domain.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+importtudai.integrador3.service.dto.estudiante.estudianteResponse.EstudianteResponseDTO;
 
 import java.util.List;
 import java.util.Optional;
 @Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante, Integer> {
 
-    @Query("SELECT e FROM Estudiante e WHERE e.LU = :LU")
-    Optional<Estudiante> buscarEstudianteLU(int LU);
+    @Query("SELECT new tudai.integrador3.service.dto.estudiante.estudianteResponse.EstudianteResponseDTO(e.DNI, e.nombre, e.apellido,e.edad, e.genero, e.ciudad, e.LU) FROM Estudiante e WHERE e.LU = :LU")
+    Optional<EstudianteResponseDTO> buscarEstudianteLU(int LU);
 
-    @Query("SELECT e FROM Estudiante e WHERE e.genero = :genero")
-    List<Estudiante> buscarEstudiantesGenero(String genero);
+    @Query("SELECT new tudai.integrador3.service.dto.estudiante.estudianteResponse.EstudianteResponseDTO(e.DNI, e.nombre, e.apellido,e.edad, e.genero, e.ciudad, e.LU) FROM Estudiante e WHERE e.genero = :genero")
+    List<EstudianteResponseDTO> buscarEstudiantesGenero(String genero);
 
-    @Query("SELECT e FROM Estudiante e ORDER BY e.apellido")
-    List<Estudiante> buscarEstudiantesApellido();
+    @Query("SELECT new tudai.integrador3.service.dto.estudiante.estudianteResponse.EstudianteResponseDTO(e.DNI, e.nombre, e.apellido,e.edad, e.genero, e.ciudad, e.LU) FROM Estudiante e ORDER BY e.apellido")
+    List<EstudianteResponseDTO> buscarEstudiantesApellido();
 
-    @Query("SELECT e FROM Estudiante e JOIN e.carreras ec " +
+    @Query("SELECT new tudai.integrador3.service.dto.estudiante.estudianteResponse.EstudianteResponseDTO(e.DNI, e.nombre, e.apellido,e.edad, e.genero, e.ciudad, e.LU) FROM Estudiante e JOIN e.carreras ec " +
             "WHERE ec.carrera.carrera = :carrera AND e.ciudad = :ciudad")
-    List<Estudiante> buscarEstudiantesCarreraCiudad(String carrera, String ciudad);
+    List<EstudianteResponseDTO> buscarEstudiantesCarreraCiudad(String carrera, String ciudad);
 
 }
